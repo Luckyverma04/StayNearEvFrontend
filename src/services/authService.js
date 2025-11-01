bb390694213a10d3d15ca1d4a577a2d2647b229a
Lucky Verma <luckykishoreverma0404@gmail.com>
2025-11-01 21:45:21 +0530

    ok

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,7 +3,7 @@ import api from './api';
 export const authService = {
   // ✅ Login user
   async login(email, password) {
-    const response = await api.post('/api/users/login', { email, password });
+    const response = await api.post('/users/login', { email, password });
     return response.data;
   },
 
